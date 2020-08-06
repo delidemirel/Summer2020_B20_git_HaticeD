e3583683a6e2105e754c96827d4a0912c89fcf67
delidemirel <68973671+delidemirel@users.noreply.github.com>
2020-08-05 20:33:19 -0500

    Update MissJava.java

--- src/utility/MissJava.java
+++ src/utility/MissJava.java
@@ -3,5 +3,8 @@ package utility;
 public class MissJava {
     public static void main(String[] args) {
         System.out.println("I miss Java");
+        int a=10;
+        int b=20;
+        System.out.println(a+b);
     }
 }
